e961a0763c472ed51d96ffcdf8e838842593cc22
thisisaaronland <aaron@mapzen.com>
2015-10-13 01:31:42 +0000

    wrangling; simple stuff works; stuck in interface hell

--- bin/index.go
+++ bin/index.go
@@ -14,9 +14,20 @@ func main() {
 	p := pip.PointInPolygon()
 
 	for _, path := range args {
-		fmt.Println(path)
-		p.ImportFile(path)
+		// fmt.Println(path)
+		p.IndexGeoJSONFile(path)
 	}
 
-	fmt.Println(p.Rtree.Size())
+	fmt.Printf("indexed %d records\n", p.Rtree.Size())
+
+	lat := 37.791614
+	lon := -122.392375
+
+	fmt.Printf("get by lat lon %f, %f\n", lat, lon)
+
+	results := p.GetByLatLon(lat, lon)
+
+	for i, r := range results {
+		fmt.Printf("result #%d is %s\n", i, r)
+	}		
 }
--- pip.go
+++ pip.go
@@ -18,7 +18,7 @@ func PointInPolygon() *WOFPointInPolygon {
 	}
 }
 
-func (wof WOFPointInPolygon) ImportFile(source string) error {
+func (p WOFPointInPolygon) IndexGeoJSONFile(source string) error {
 
 	feature, parse_err := geojson.UnmarshalFile(source)
 
@@ -26,22 +26,26 @@ func (wof WOFPointInPolygon) ImportFile(source string) error {
 		return parse_err
 	}
 
+	return p.IndexGeoJSONFeature(*feature)
+}
+
+func (p WOFPointInPolygon) IndexGeoJSONFeature(feature geojson.WOFFeature) error {
+
 	bounds, bounds_err := feature.Bounds()
 
 	if bounds_err != nil {
 		return bounds_err
 	}
 
-	wof.Rtree.Insert(bounds)
+	p.Rtree.Insert(bounds)
 	return nil
 }
 
-/*
-func (wof WOFPointInPolygon) LookupPoint(lat float64, lon float64) {
+func (p WOFPointInPolygon) GetByLatLon(lat float64, lon float64) []rtreego.Spatial{
 
-	q := rtreego.Point{lat, lon}
-	k := 5
+	pt := rtreego.Point{lon, lat}
+	bbox, _ := rtreego.NewRect(pt, []float64{0.0001, 0.0001})	// how small can I make this?
 
-	results = wof.Rtree.SearchNearestNeighbors(q, k)
+	results := p.Rtree.SearchIntersect(bbox)
+	return results
 }
-*/
