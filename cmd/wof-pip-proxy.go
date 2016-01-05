1f7f6b0e12ddba4da40442a1f8f35c5918bebeb4
thisisaaronland <aaron@mapzen.com>
2016-01-05 00:52:23 +0000

    start to read target spec from a config file

--- cmd/wof-pip-proxy.go
+++ cmd/wof-pip-proxy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,72 +11,127 @@ import (
 	"strings"
 )
 
+type WOFProxyTargets map[string]WOFProxyTarget
+
+type WOFProxyTarget struct {
+	Target string
+	Port   int
+}
+
+type WOFProxyHandler func(rsp http.ResponseWriter, req *http.Request)
+
 func main() {
 
 	var host = flag.String("host", "localhost", "The hostname to listen for requests on")
 	var port = flag.Int("port", 1111, "The port number to listen for requests on")
+	var config = flag.String("config", "", "")
 
 	flag.Parse()
 
 	endpoint := fmt.Sprintf("%s:%d", *host, *port)
 
-	proxyHandler := http.HandlerFunc(proxyHandlerFunc)
-	http.ListenAndServe(endpoint, proxyHandler)
-}
+	// Check if config == "-" and read from flag.Args()
 
-func proxyHandlerFunc(rsp http.ResponseWriter, req *http.Request) {
-
-	targets := make(map[string]int)
-	targets["locality"] = 6666
-
-	p := req.URL.Path
-	p = strings.Replace(p, "/", "", 1)
-
-	if p == "" {
-		http.Error(rsp, "Missing target", http.StatusBadRequest)
-		return
-	}
-
-	port, ok := targets[p]
-
-	if !ok {
-		http.Error(rsp, "Invalid target", http.StatusBadRequest)
-		return
-	}
-
-	host := fmt.Sprintf("http://localhost:%s", strconv.Itoa(port))
-	url := host + "?" + req.URL.RawQuery
-
-	body := bytes.NewBuffer([]byte(""))
-
-	_req, err := http.NewRequest("GET", url, body)
+	spec, err := ioutil.ReadFile(*config)
 
 	if err != nil {
-		http.Error(rsp, err.Error(), http.StatusBadRequest)
-		return
+	   panic(err)
 	}
 
-	client := &http.Client{}
-	_rsp, err := client.Do(_req)
+	targets, err := proxyHandlerTargets(spec)
 
 	if err != nil {
-		http.Error(rsp, err.Error(), http.StatusBadRequest)
-		return
+		panic(err)
 	}
 
-	for k, v := range _rsp.Header {
-		for _, vv := range v {
-			rsp.Header().Add(k, vv)
+	for t, p := range targets {
+		if p.Port == *port {
+			err := fmt.Sprintf("Target port (%s:%d) is the same as proxy port", t, *port)
+			panic(err)
 		}
 	}
 
-	rsp.WriteHeader(_rsp.StatusCode)
-	result, err := ioutil.ReadAll(_rsp.Body)
+	handler := proxyHandlerFunc(targets)
+
+	proxyHandler := http.HandlerFunc(handler)
+	http.ListenAndServe(endpoint, proxyHandler)
+}
+
+func proxyHandlerTargets(spec []byte) (WOFProxyTargets, error) {
+
+	var pt []WOFProxyTarget
+
+	err := json.Unmarshal(spec, &pt)
 
 	if err != nil {
-		http.Error(rsp, err.Error(), http.StatusBadRequest)
-		return
+		return nil, err
 	}
 
-	rsp.Write(result)
+	targets := WOFProxyTargets{}
+
+	for _, p := range pt {
+		targets[p.Target] = p
+	}
+
+	return targets, nil
+}
+
+func proxyHandlerFunc(targets WOFProxyTargets) WOFProxyHandler {
+
+	// See this - it's basically just so that we can scope
+	// targets to the handler function (20160104/thisisaaronland)
+
+	return func(rsp http.ResponseWriter, req *http.Request) {
+
+		p := req.URL.Path
+		p = strings.Replace(p, "/", "", 1)
+
+		if p == "" {
+			http.Error(rsp, "Missing target", http.StatusBadRequest)
+			return
+		}
+
+		target, ok := targets[p]
+
+		if !ok {
+			http.Error(rsp, "Invalid target", http.StatusBadRequest)
+			return
+		}
+
+		host := fmt.Sprintf("http://localhost:%s", strconv.Itoa(target.Port))
+		url := host + "?" + req.URL.RawQuery
+
+		body := bytes.NewBuffer([]byte(""))
+
+		_req, err := http.NewRequest("GET", url, body)
+
+		if err != nil {
+			http.Error(rsp, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		client := &http.Client{}
+		_rsp, err := client.Do(_req)
+
+		if err != nil {
+			http.Error(rsp, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		for k, v := range _rsp.Header {
+			for _, vv := range v {
+				rsp.Header().Add(k, vv)
+			}
+		}
+
+		rsp.WriteHeader(_rsp.StatusCode)
+		result, err := ioutil.ReadAll(_rsp.Body)
+
+		if err != nil {
+			http.Error(rsp, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		rsp.Write(result)
+	}
 }
