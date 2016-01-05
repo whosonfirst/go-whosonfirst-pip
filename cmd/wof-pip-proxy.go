1d9a1b4a37ee253c17f6e196ef522fcba635d22e
thisisaaronland <aaron@mapzen.com>
2016-01-05 02:21:38 +0000

    add local Ping and URL methods; read config from STDIN

--- cmd/wof-pip-proxy.go
+++ cmd/wof-pip-proxy.go
@@ -1,21 +1,64 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	_ "bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
+	"os"
+	_ "strconv"
 	"strings"
 )
 
-type WOFProxyTargets map[string]WOFProxyTarget
+type WOFProxyTargets map[string]*WOFProxyTarget
 
 type WOFProxyTarget struct {
 	Target string
 	Port   int
+	Host   string
+	Meta   string
+}
+
+func (pt *WOFProxyTarget) URL() string {
+
+	scheme := "http"
+	host := "localhost"
+
+	if pt.Host != "" {
+		host = pt.Host
+	}
+
+	port := pt.Port
+
+	return fmt.Sprintf("%s://%s:%d", scheme, host, port)
+}
+
+func (pt *WOFProxyTarget) Ping() (bool, error) {
+
+	/*
+	   Note that wof-pip-server does not have a true 'ping' endpoint
+	   so we are just checking that it returns anything at all
+	   (20160104/thisisaaronland)
+	*/
+
+	test := pt.URL()
+	req, err := http.NewRequest("HEAD", test, nil)
+
+	if err != nil {
+		return false, err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(req)
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
 
 type WOFProxyHandler func(rsp http.ResponseWriter, req *http.Request)
@@ -24,18 +67,35 @@ func main() {
 
 	var host = flag.String("host", "localhost", "The hostname to listen for requests on")
 	var port = flag.Int("port", 1111, "The port number to listen for requests on")
-	var config = flag.String("config", "", "")
+	var config = flag.String("config", "", "... (If the value is - then read the config from STDIN)")
 
 	flag.Parse()
 
 	endpoint := fmt.Sprintf("%s:%d", *host, *port)
 
-	// Check if config == "-" and read from flag.Args()
+	var spec []byte
 
-	spec, err := ioutil.ReadFile(*config)
+	if *config == "-" {
 
-	if err != nil {
-	   panic(err)
+		var raw string
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			raw += scanner.Text()
+		}
+
+		spec = []byte(raw)
+
+	} else {
+
+		_spec, err := ioutil.ReadFile(*config)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// Oh Go...
+		spec = _spec
 	}
 
 	targets, err := proxyHandlerTargets(spec)
@@ -45,15 +105,37 @@ func main() {
 	}
 
 	for t, p := range targets {
+
 		if p.Port == *port {
 			err := fmt.Sprintf("Target port (%s:%d) is the same as proxy port", t, *port)
 			panic(err)
 		}
+
+		/*
+
+			You might be thinking: Oh wouldn't it be nice to spawn a series of subprocesses
+			but it appears as though Go deliberately chooses _not_ to support subprocesses
+			which is kind of weird but there you go... Maybe it's possible and I just don't
+			know the magic incantation but for now we're going to assume that starting the
+			individual servers is something else's problem... Good times.
+			(20160104/thisisaaronland)
+
+		*/
+
+		ok, err := p.Ping()
+
+		if !ok {
+			msg := fmt.Sprintf("Target (%s:%d) is not awake or connected to the network: %v", p.Host, p.Port, err)
+			panic(msg)
+		}
+
 	}
 
 	handler := proxyHandlerFunc(targets)
-
 	proxyHandler := http.HandlerFunc(handler)
+
+	fmt.Println(endpoint)
+
 	http.ListenAndServe(endpoint, proxyHandler)
 }
 
@@ -70,7 +152,8 @@ func proxyHandlerTargets(spec []byte) (WOFProxyTargets, error) {
 	targets := WOFProxyTargets{}
 
 	for _, p := range pt {
-		targets[p.Target] = p
+
+		targets[p.Target] = &p
 	}
 
 	return targets, nil
@@ -98,12 +181,11 @@ func proxyHandlerFunc(targets WOFProxyTargets) WOFProxyHandler {
 			return
 		}
 
-		host := fmt.Sprintf("http://localhost:%s", strconv.Itoa(target.Port))
-		url := host + "?" + req.URL.RawQuery
+		// please just make me a url.URL thingy...
 
-		body := bytes.NewBuffer([]byte(""))
+		url := target.URL() + "?" + req.URL.RawQuery
 
-		_req, err := http.NewRequest("GET", url, body)
+		_req, err := http.NewRequest("GET", url, nil)
 
 		if err != nil {
 			http.Error(rsp, err.Error(), http.StatusBadRequest)
