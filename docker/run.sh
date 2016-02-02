339c108ac556f9f3bdce3f7f2c758289446d694a
Grant Heffernan <heffergm@gmail.com>
2016-02-02 15:21:58 -0500

    Revert "only_if"
    This reverts commit 176f425861c769bcd2bf6cc15f3d7fd83dc1d835.

--- docker/run.sh
+++ docker/run.sh
@@ -6,20 +6,16 @@ for f in ${METAFILES}; do
   # download data
   echo "Now working on ${f}:"
 
-  if [ -f "${METADIR}/${f}.csv" ]; then
-    echo -e "\t...metadata already exists, skipping..."
-  else
-    cd ${METADIR}
-    echo -e "\t...pulling metadata..."
-    wget --quiet -O ${f}.csv ${SOURCEURL}/bundles/${f}.csv
+  cd ${METADIR}
+  echo -e "\t...pulling metadata..."
+  wget --quiet -O ${f}.csv ${SOURCEURL}/bundles/${f}.csv
 
-    cd ${DATADIR}
-    echo -e "\t...pulling data..."
-    wget --quiet -O ${f}.tar.bz2 ${SOURCEURL}/bundles/${f}-bundle.tar.bz2
+  cd ${DATADIR}
+  echo -e "\t...pulling data..."
+  wget --quiet -O ${f}.tar.bz2 ${SOURCEURL}/bundles/${f}-bundle.tar.bz2
 
-    echo -e "\t...extracting data..."
-    bunzip2 -f ${f}.tar.bz2 && tar xf ${f}.tar --strip-components=2 && rm ${f}.tar
-  fi
+  echo -e "\t...extracting data..."
+  bunzip2 -f ${f}.tar.bz2 && tar xf ${f}.tar --strip-components=2 && rm ${f}.tar
 done
 
 cd ${INSTALLDIR}
