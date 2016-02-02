5746728a7037951396f3e282016a3d994993fb6d
Grant Heffernan <heffergm@gmail.com>
2016-02-02 15:21:32 -0500

    Revert "update data method"
    This reverts commit ba04d1d191b3927256b92aaffbc8a1ce2a8054c3.

--- docker/run.sh
+++ docker/run.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-
-get_data() {
-  cd ${METADIR}
-  echo -e "\t...pulling metadata..."
-  wget --quiet -O ${f}.csv ${SOURCEURL}/bundles/${f}.csv
-
-  cd ${DATADIR}
-  echo -e "\t...pulling data..."
-  wget --quiet -O ${f}.tar.bz2 ${SOURCEURL}/bundles/${f}-bundle.tar.bz2
-
-  echo -e "\t...extracting data..."
-  bunzip2 -f ${f}.tar.bz2 && tar xf ${f}.tar --strip-components=2
-}
-
 for f in ${METAFILES}; do
   # construct the metafile args
   csv="${METADIR}/${f}.csv ${csv}"
@@ -20,14 +6,19 @@ for f in ${METAFILES}; do
   # download data
   echo "Now working on ${f}:"
 
-  if [[ ${UPDATEDATA} -eq "false" ]]; then
-    if [ -f "${DATADIR}/${f}.tar" ]; then
-      echo -e "\t...data exists, skipping..."
-    else
-      get_data
-    fi
+  if [ -f "${DATADIR}/${f}.tar" ]; then
+    echo -e "\t...data exists, skipping..."
   else
-    get_data
+    cd ${METADIR}
+    echo -e "\t...pulling metadata..."
+    wget --quiet -O ${f}.csv ${SOURCEURL}/bundles/${f}.csv
+
+    cd ${DATADIR}
+    echo -e "\t...pulling data..."
+    wget --quiet -O ${f}.tar.bz2 ${SOURCEURL}/bundles/${f}-bundle.tar.bz2
+
+    echo -e "\t...extracting data..."
+    bunzip2 -f ${f}.tar.bz2 && tar xf ${f}.tar --strip-components=2
   fi
 done
 
